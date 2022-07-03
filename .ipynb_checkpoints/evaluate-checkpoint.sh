5b5fd407c65e970beee7a6069696c5a6f0e537d2
dloading7 <dloading7@163.com>
2022-07-03 12:58:51 +0800

    .

--- .ipynb_checkpoints/evaluate-checkpoint.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-CUDA_VISIBLE_DEVICES=2 python benchmark_3dmatch.py --source /data/ghn/FCGF_data/threedmatch_test --target ./features_tmp --num_rand_keypoints 5000 \
-              --voxel_size 0.025 --model ./snapshot/HardestContrastiveLoss_11271549/checkpoints/model_best_recall.pth \
-              --evaluate_fmr
--- .ipynb_checkpoints/run-checkpoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-CUDA_VISIBLE_DEVICES=0 python main.py \
-	--loss_type HardestContrastiveLoss \
-	--model ResUNet_spconv \
-	--num_workers 6 \
-	--lr 1e-4
